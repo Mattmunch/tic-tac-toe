d325a6d0324b008eb3823140ae50d49076b6de34
Matt Munch <mattmunch94@gmail.com>
2020-03-06 11:59:52 -0800

    square component for game board fleshed out

---
+++ src/components/Square.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Square = () => (
+  <button className='square'>
+    {/* TODO */}
+  </button>
+);
+
+Square.propTypes = {};
+
+export default Square;
+
+
